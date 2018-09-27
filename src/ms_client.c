3083932694922955eb62aae22f3d648735e1e438
Grubsta <coreywilliam21@outlook.com>
2018-09-27 18:05:51 +1000

    Added basic function skeletons to server and client

--- src/ms_client.c
+++ src/ms_client.c
@@ -1,1 +1,56 @@
-// Minesweeper client skeleton
+// Minesweeper client skeleton.
+
+// Created by:
+// Jack (n)
+// Corey (n10007164)
+
+#include "includes.h"
+
+/*
+* Initialises the games playable field.
+*/
+void initField() {
+
+}
+
+/*
+* Initialises the game.
+*/
+void initGame() {
+
+}
+
+/*
+* Game processes.
+*/
+void gameProcess() {
+
+}
+
+/*
+* Outputs the leaderboard.
+*/
+void leaderBoard() {
+
+}
+
+/*
+* Finishs the connections between user and server.
+*/
+void finish() {
+
+}
+
+/*
+* Login functionality.
+*/
+void Login() {
+
+}
+
+/*
+* Main.
+*/
+int main(int argc, char *argv[]) {
+
+}
--- src/ms_server.c
+++ src/ms_server.c
@@ -1,1 +1,44 @@
-// Minesweeper server skeleton
+// Minesweeper server skeleton.
+
+#include "includes.h"
+
+/*
+*
+*/
+typedef struct {
+  int adjacement_mines;
+  bool visable;
+  bool is_mine;
+} Tile;
+
+/*
+* Stores required variables of a users game.
+*/
+typedef struct GameState {
+  // Add more stored variables
+  Tile tiles [NUM_TILES_X][NUM_TILES_Y];
+};
+
+/*
+* Reads a text file and outputs the results to an array.
+*/
+int reader() {
+
+}
+
+/*
+* Shut downs server cleanly.
+* Handles threads, open sockets, dynamically allocated memory and open files.
+*/
+void shutDown() {
+
+}
+
+/*
+* Main.
+*/
+int main(int argc, char *argv[]) {
+  srand(SEED);
+  reader();
+
+}
