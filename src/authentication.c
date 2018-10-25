e110bd969d4023177623c88546a07715ebbc96e0
Grubsta <coreywilliam21@outlook.com>
2018-10-25 15:14:59 +1000

    Added authentication file

---
+++ src/authentication.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+// Creates a Hash Table of 10 users
+// userTable = (struct hash *) calloc(10, sizeof(struct hash));
+
+int GenerateUsers() {
+  // Verify file exists, and open it
+  FILE * file = fopen("Authentication.txt", "r");
+  if (file == NULL) {
+    printf("Failed to locate authentication.txt. Please check filepath.\n");
+    return 1;
+  }
+
+  char * line = NULL;
+  int currentLine = 0;
+  size_t length = 0;
+  int key = 0;
+  char * user = NULL;
+  char * pass = NULL;
+
+
+  // Loop through .txt line by line
+  while ((getline(&line, &length, file)) != -1) {
+    currentLine++;
+
+    char * token = strtok(line, "\t\n\r");
+    int tokenCounter = 0;
+
+    // Split on TAB delimiter until end of line
+    while (token != NULL) {
+      if (tokenCounter == 0) {
+        user = malloc(strlength(token) * sizeof(char));
+        strcpy(user, token);
+      }
+      else if (tokenCounter == 1) {
+        pass = malloc(strlength(token) * sizeof(char));
+        strcpy(pass, token);
+      }
+      token = strtok(NULL, "\t\n\r");
+      tokenCounter++;
+    }
+
+    // Catch error in file and terminate program
+    if (user == NULL || pass == NULL) {
+      printf("Invalid format Line %d.\n. The program will now terminate.", currentLine + 1);
+      fclose(file);
+      return 0;
+    }
+    if (user != "Username"){
+      printf("U: %sP: %s\n", user, pass);
+    }
+
+  }
+  printf("%s, %d, %ld, %d\n", line, currentLine, length, key);
+  fclose(file);
+  return 1;
+}
+
+int main()
+{
+	int loaded = GenerateUsers();
+}
